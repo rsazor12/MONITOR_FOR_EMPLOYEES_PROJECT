f7f2804203835febb8fa53cbf3d0168b0b0d0733
rsazor12 <bielskikamil@gmail.com>
2017-06-17 11:35:16 +0200

    Dodaj pliki projektów.

---
+++ MONITOR_FOR_EMPLOYEES_PROJECT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MONITOR_FOR_EMPLOYEES_PROJECT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
