9992752eb1da6b9c2d13c28c08d4091c343200b9
cze-aga <cze-aga@o2.pl>
2017-06-19 15:23:53 +0200

    Dodanie kalsy MouseMonitor. Liczy kliknięcia LPM,PPM, podaje pozycje gdzie kliknięto. Podczas SCROLL wypisuje "Scroll".

---
+++ MONITOR_FOR_EMPLOYEES_PROJECT/MonitoringClasses/MouseMonitorClass/MouseMonitorClass.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MONITOR_FOR_EMPLOYEES_PROJECT.MonitoringClasses.MouseMonitorClass
+{
+     static class MouseMonitorClass
+    {
+        public static LowLevelMouseProc _proc = HookCallback;//
+        public static IntPtr _hookID = IntPtr.Zero; //inicjalizacja intem=0
+        public static int lbutton = 0;
+        public static int rbutton = 0;
+
+     /*   public static void Main()
+        {
+            _hookID = SetHook(_proc);
+
+            Application.Run();
+            UnhookWindowsHookEx(_hookID);
+        }*/
+
+
+        public static IntPtr SetHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+
+        public delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+
+        public static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam)
+            {
+                MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                Console.WriteLine(hookStruct.pt.x + ", " + hookStruct.pt.y + "\tkliknięcia LPM: " + ++lbutton);
+
+            }
+            else if (nCode >= 0 && MouseMessages.WM_RBUTTONDOWN == (MouseMessages)wParam)
+            {
+                MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                Console.WriteLine(hookStruct.pt.x + ", " + hookStruct.pt.y + "\tkliknięcia PPM: " + ++rbutton);
+            }
+            else if (nCode >= 0 && MouseMessages.WM_MOUSEWHEEL == (MouseMessages)wParam)
+            {
+
+                Console.WriteLine("Scroll");
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+
+        private const int WH_MOUSE_LL = 14;
+
+
+        private enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+
+        private struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+        public static extern short GetKeyState(int keyCode);
+    }
+
+}
