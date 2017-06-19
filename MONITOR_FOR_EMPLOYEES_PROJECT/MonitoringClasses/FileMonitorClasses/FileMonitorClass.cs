a70f92db8aac93ae6546c8ebdae47d51104d080b
rsazor12 <bielskikamil@gmail.com>
2017-06-19 18:10:13 +0200

    Dodanie Klasy FileMonitorClass

---
+++ MONITOR_FOR_EMPLOYEES_PROJECT/MonitoringClasses/FileMonitorClasses/FileMonitorClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MONITOR_FOR_EMPLOYEES_PROJECT.MonitoringClasses.FileMonitorClasses
+{
+    class FileMonitorClass
+    {
+
+    }
+}
