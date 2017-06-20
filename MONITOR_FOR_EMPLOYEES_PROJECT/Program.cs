f653621c6423d8f1992e3808f262a0ea6a7bd4e0
rsazor12 <bielskikamil@gmail.com>
2017-06-20 17:29:09 +0200

    Testoe łączenie z bazą

--- MONITOR_FOR_EMPLOYEES_PROJECT/Program.cs
+++ MONITOR_FOR_EMPLOYEES_PROJECT/Program.cs
@@ -16,14 +16,33 @@ namespace MONITOR_FOR_EMPLOYEES_PROJECT
         {
             //Tu Tworz
             FileMonitorClass obFileMonitorClass = new FileMonitorClass("c:\\Workspace");
-            
+
 
             /*MouseMonitorClass._hookID = MouseMonitorClass.SetHook(MouseMonitorClass._proc);
             Application.Run();
             MouseMonitorClass.UnhookWindowsHookEx(MouseMonitorClass._hookID);
             Console.ReadKey();//push1*/
 
+            connect();
+        }
 
+        public static void connect()
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=149.156.136.151:3306;uid=kbielski;" + "pwd=1234567890;database=kbielski;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
