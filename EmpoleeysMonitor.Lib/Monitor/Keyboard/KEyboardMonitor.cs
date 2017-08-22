6a220b1af2874b2b9a95e1ae0affc9da28c96341
wojciech.czajkowski <wojciech.czajkowski@comarch.pl>
2017-08-22 22:57:20 +0200

    Poprawki w keyboard monitorze

--- EmpoleeysMonitor.Lib/Monitor/Keyboard/KEyboardMonitor.cs
+++ EmpoleeysMonitor.Lib/Monitor/Keyboard/KEyboardMonitor.cs
@@ -2,28 +2,22 @@
 using EmployeesMonitor.Lib.Monitor;
 using Keystroke.API;
 using Keystroke.API.CallbackObjects;
-using SaveToFile;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EmpoleeysMonitor.Lib.Monitor.Sample
 {
     public class KeyboardMonitor : IMonitor
     {
-        private static string KeyString = String.Empty;
         private object locker = new object();
         private List<UserAction> actions = new List<UserAction>();
-        private Random random = new Random();
         private Thread thread;
 
         public void Start()
         {
-            thread = new Thread(GenerateSampleData);
+            thread = new Thread(Init);
             thread.Start();
         }
 
@@ -42,34 +36,27 @@ namespace EmpoleeysMonitor.Lib.Monitor.Sample
             }
         }
 
-        private void GenerateSampleData()
+        private void Init()
         {
             using (var api = new KeystrokeAPI())
             {
                 api.CreateKeyboardHook(KeyPressedCallback);
                 Application.Run();
             }
-
-
-            while (true)
-            {
-                lock (locker)
-                {
-                    actions.Add(new UserAction()
-                    {
-                        ActionType = ActionType.KeyboardPressed,
-                        Date = DateTime.UtcNow,
-                        Info = KeyString
-                    });
-                }
-            }
         }
 
 
-        private static void KeyPressedCallback(KeyPressed obj)
+        private void KeyPressedCallback(KeyPressed obj)
         {
-            KeyString += obj.ToString();
-            //Console.WriteLine(MyStorage.logStorage);
+            lock (locker)
+            {
+                actions.Add(new UserAction()
+                {
+                    ActionType = ActionType.KeyboardPressed,
+                    Date = DateTime.UtcNow,
+                    Info = obj.ToString()
+                });
+            }
         }
     }
 }
